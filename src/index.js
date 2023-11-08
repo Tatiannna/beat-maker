c515f9239707ae83c633bdadc2826a8d9c6d68f6
Tatianna <tatirobinson@gmail.com>
2023-11-08 00:02:44 -0800

    Refactor track/control/segment div/element hierarchy

--- src/index.js
+++ src/index.js
@@ -8,21 +8,19 @@ newTrackButton.textContent = 'Add Track';
 nav.appendChild(newTrackButton);
 
 
-const track = document.createElement('ul');
+//Track container
+const track = document.createElement('div');
 track.classList.add("track");
+container.appendChild(track);
 
-// Add controls
-    let remove = document.createElement('button');
-    remove.textContent = 'Remove';
-    let mute = document.createElement('button');
-    mute.textContent = 'Mute';
-    track.appendChild(remove);
-    track.appendChild(mute);
+//Right track div
+const trackRight = document.createElement('div');
+trackRight.classList.add("track-div-right")
+track.appendChild(trackRight);
 
 
-
-
-//Add segments
+//Add segments to right track div
+const trackUL = document.createElement('ul');
 for (let i = 0; i < 8; i++){
     let segment = document.createElement('li');
     segment.classList.add("segment");
@@ -30,8 +28,25 @@ for (let i = 0; i < 8; i++){
     segment.dataset.pos = `${i}`;
     track.appendChild(segment);
 }
+trackRight.appendChild(trackUL);
 
-container.appendChild(track);
+
+
+// create Left track div
+const trackLeft = document.createElement('div');
+trackLeft.classList.add("track-div-left");
+track.appendChild(trackLeft);
+
+// Add controls to left track div
+    let remove = document.createElement('button');
+    remove.textContent = 'Remove';
+    let mute = document.createElement('button');
+    mute.textContent = 'Mute';
+    trackLeft.appendChild(remove);
+    trackLeft.appendChild(mute);
+
+
+
 
 let segmentState = false;
 
@@ -51,15 +66,12 @@ track.addEventListener("click", (e) =>{
 });
 
 
-
-
-
-
-
 newTrackButton.addEventListener("click", (e) =>{
+
     //Create Track
-    let track = document.createElement('ul');
-    track.classList.add("track");
+
+    let trackUL = document.createElement('ul');
+    //trackUL.classList.add("track");
 
     
     //Add controls
