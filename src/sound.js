84f856f3e8a891b542b5d855b4ec4ef4c71e8f7e
Tatianna <tatirobinson@gmail.com>
2023-11-10 12:34:40 -0800

    Create instance variable to store Sound Pack names

--- src/sound.js
+++ src/sound.js
@@ -1,23 +1,43 @@
 
 
-
 class Sound {
     constructor(){
-        this.audio = new Audio("./assets/RD_C_1.wav");
+        //this.track = track;
+        this.audio = new Audio("./assets/Claps/RD_C_1.wav");
+        this.packs = [
+            {name: "Claps", count: 2}, 
+            {name: "Blast Block", count: 3}, 
+            {name: "Egg Shaker", count: 3}, 
+            {name: "Mid Tom", count: 3}
+        ];
     }
 
     playSound(){
-        console.log("playing sound");
         this.audio.play();
+    }
+
+    getCollections(){
+        const collectionPaths = [];
+
+        for(let i = 0; i < this.collectionNames.length; i++){
+            collectionPaths.push({path: `./assets/${collectionNames[i]}/`, name: collectionNames[i]});
+        }
+        return collectionPaths;
+    }
+
+    getSoundPath(collection, option){
+        return collection + option;
+    }
+
+    setSound(){
+
     }
 
     random(){
 
     }
-
-    set(){
-
-    }
 }
+
+
 
 export default Sound;
