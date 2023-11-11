303295c0997b580f9d52ea6d0ddf9fb8fd347636
Tatianna <tatirobinson@gmail.com>
2023-11-11 13:53:19 -0800

    Create function to play all tracksgit status

--- src/beat.js
+++ src/beat.js
@@ -6,18 +6,27 @@ class Beat {
         this.tempo = 1000;
         this.volume = 1000;
         this.tracks = [new Track(container)];
-        console.log("All tracks:" , this.tracks)
+        //console.log("All tracks:" , this.tracks)
     }
 
     addTrack(container){
         const track = new Track(container);
         this.tracks.push(track);
-        console.log("All tracks:" , this.tracks)
+        // console.log("All tracks:" , this.tracks)
     }
 
 
     playAllTracks(){
+        const playFunctions = [];
+
         console.log(this.tracks);
+
+        this.tracks.forEach( (track) => {
+            playFunctions.push(new Promise(track.playTrack.bind(track)));
+        })
+        console.log(playFunctions);
+
+        Promise.all(playFunctions).catch(() => console.log("Something went wrong!"));
     }
 
     changeTempo(diff){
